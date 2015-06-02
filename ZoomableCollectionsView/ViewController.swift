7d0cb08a449d19cb18d5b698e69377a8f8bc5f77
Josiah Gaskin <josiahgaskin@gmail.com>
2015-06-01 21:55:29 -0700

    Initial zooming works

--- ZoomableCollectionsView/ViewController.swift
+++ ZoomableCollectionsView/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    let flowLayout = UICollectionViewFlowLayout()
+    var baseSize : CGSize!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.setCollectionViewLayout(flowLayout, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
 
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("imagecell", forIndexPath: indexPath) as! SelectableImageCell
+        
+        let nameIndex = indexPath.row % 13 + 1
+        cell.image.image = UIImage(named: "\(nameIndex)")
+        return cell
+    }
+
+    @IBAction func didPinch(sender: UIPinchGestureRecognizer) {
+        switch sender.state {
+        case .Began:
+            baseSize = flowLayout.itemSize
+        case .Ended, .Cancelled, .Changed:
+            flowLayout.itemSize = CGSizeMake(baseSize.width * sender.scale, baseSize.height * sender.scale)
+        default:
+            return
+        }
+    }
 }
 
+class SelectableImageCell : UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+}
